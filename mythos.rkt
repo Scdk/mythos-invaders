e656308e5255a80cfab0b55ea52933625f0edf1f
Gap1512 <gap1512@gmail.com>
2018-10-22 11:01:53 -0300

    Implemented data structure monster

--- mythos.rkt
+++ mythos.rkt
@@ -4,16 +4,19 @@
 (require 2htdp/universe)
 (require 2htdp/image)
 
-(define PROPORTION 0.2)
-(define RESOLUTION 256)
-(define CENTER (make-posn (/ (* RESOLUTION PROPORTION) 2) (/ (* RESOLUTION PROPORTION) 2)))
+(define PROPORTION 0.1)
+(define RESOLUTION (* 256 PROPORTION))
+(define CENTER (make-posn (/ RESOLUTION 2) (/ RESOLUTION 2)))
 (define MAX-HEALTH 8)
-(define EMPTY (empty-scene (* RESOLUTION PROPORTION) (* RESOLUTION PROPORTION)))
+(define EMPTY (empty-scene RESOLUTION RESOLUTION))
 
 ;;Data Structures
 
 ;A Mythos has two frames and the position of the center and represents the enemies
-(define-struct mythos [frame1 frame2 center pos])
+(define-struct mythos [frame1 frame2 center])
+
+;A Monster has a number, a type and a position
+(define-struct monster [number type pos])
 
 ;A Barrier has eight frames and the position of the center
 (define-struct barrier [frame1
@@ -39,28 +42,22 @@
 ;Each model type of monster is defined, using make-struct
 (define CTHULHU (make-mythos (scale PROPORTION (bitmap "sprites/Cthulhu-1.png"))
                              (scale PROPORTION (bitmap "sprites/Cthulhu-2.png"))
-                             CENTER
-                             (make-posn 0 0)))
+                             CENTER))
 (define BROWN-JENKIN (make-mythos (scale PROPORTION (bitmap "sprites/Brown Jenkin-1.png"))
                              (scale PROPORTION (bitmap "sprites/Brown Jenkin-2.png"))
-                             CENTER
-                             (make-posn 0 0)))
+                             CENTER))
 (define DAGON (make-mythos (scale PROPORTION (bitmap "sprites/Dagon-1.png"))
                              (scale PROPORTION (bitmap "sprites/Dagon-2.png"))
-                             CENTER
-                             (make-posn 0 0)))
+                             CENTER))
 (define KASSOGHTA (make-mythos (scale PROPORTION (bitmap "sprites/Kassoghta-1.png"))
                              (scale PROPORTION (bitmap "sprites/Kassoghta-2.png"))
-                             CENTER
-                             (make-posn 0 0)))
+                             CENTER))
 (define NYARLATHOTEP (make-mythos (scale PROPORTION (bitmap "sprites/Nyarlathotep-1.png"))
                              (scale PROPORTION (bitmap "sprites/Nyarlathotep-2.png"))
-                             CENTER
-                             (make-posn 0 0)))
+                             CENTER))
 (define SHUB-NIGGURATH (make-mythos (scale PROPORTION (bitmap "sprites/Shub-Niggurath-1.png"))
                              (scale PROPORTION (bitmap "sprites/Shub-Niggurath-2.png"))
-                             CENTER
-                             (make-posn 0 0)))
+                             CENTER))
 
 ;The barrier model is defined, using make-struct
 (define HOLY-WATER (make-barrier (scale PROPORTION (bitmap "sprites/Agua Benta-1.png"))
@@ -95,23 +92,41 @@
                                           (/ (* (image-height (bitmap "sprites/Tiro.png")) PROPORTION) 2))
                                (make-posn 0 0)))
 
+;Function to define initial position
+;Number -> Posn
+;(define (initial-posn number))
+(define (initial-posn number)
+  (make-posn (+ (* 2 RESOLUTION) (posn-x CENTER) (* 2 (/ number 6) RESOLUTION))
+             (+ (* 2 RESOLUTION) (posn-y CENTER) (* 2 (remainder number 6) RESOLUTION))))
+
+;All the monsters are defined
+(define MONSTER-1-1 (make-monster 0 NYARLATHOTEP (initial-posn 0)))
+(define MONSTER-1-2 (make-monster 1 NYARLATHOTEP (initial-posn 1)))
+(define MONSTER-1-3 (make-monster 2 NYARLATHOTEP (initial-posn 2)))
+(define MONSTER-1-4 (make-monster 3 NYARLATHOTEP (initial-posn 3)))
+(define MONSTER-1-5 (make-monster 4 NYARLATHOTEP (initial-posn 4)))
+(define MONSTER-1-6 (make-monster 5 NYARLATHOTEP (initial-posn 5)))
+
 ;Shoot, Mythos -> Boolean
 ;Given a Shoot, test if some monster was hit
 ;(define (shoot-hit? shoot monster)
 (define (shoot-hit? shoot monster)
-  (and (<= (- (posn-x (mythos-pos monster)) (posn-x (mythos-center monster)))
+  (and (<= (- (posn-x (monster-pos monster)) (posn-x (mythos-center (monster-type monster))))
                (posn-x (shoot-pos shoot))
-               (+ (posn-x (mythos-pos monster)) (posn-x (mythos-center monster))))
-           (<= (- (posn-y (mythos-pos monster)) (posn-y (mythos-center monster)))
+               (+ (posn-x (monster-pos monster)) (posn-x (mythos-center (monster-type monster)))))
+           (<= (- (posn-y (monster-pos monster)) (posn-y (mythos-center (monster-type monster))))
                (posn-y (shoot-pos shoot))
-               (+ (posn-y (mythos-pos monster)) (posn-y (mythos-center monster))))))
+               (+ (posn-y (monster-pos monster)) (posn-y (mythos-center (monster-type monster)))))))
 
 ;Shoot, Mythos-List -> Boolean-List
 ;Given a Shoot, returns a list with the monsters who have died
 (define (died-monsters-after-shoot shoot list)
   (map (λ (list) (shoot-hit? shoot list)) list))
 
-;Mythos -> Image
+;Mythos Image -> Image
 ;Renders the mythos
-(define (render-mythos mythos)
-  (place-image (mythos-frame1 mythos) (posn-x (mythos-pos mythos)) (posn-y (mythos-pos mythos)) (background-frame SCENE)))
+(define (render-single-monster monster img)
+  (overlay/offset (mythos-frame1 (monster-type monster))
+                  (- (posn-x (monster-pos monster)) (posn-x (background-center SCENE)))
+                  (- (posn-y (monster-pos monster)) (posn-y (background-center SCENE)))
+                  img))
